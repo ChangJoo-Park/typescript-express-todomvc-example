f5bcfe90d94a28b822f07ff63ebbf8de6c093570
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-02-21 22:30:24 +0900

    Update index.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,29 @@
  * All application routes.
  */
 export const AppRoutes = [
+  {
+    path: '/tasks',
+    method: 'get',
+    action: ''
+  },
+  {
+    path: '/tasks',
+    method: 'post',
+    action: ''
+  },
+  {
+    path: '/tasks/:id',
+    method: 'get',
+    action: ''
+  },
+  {
+    path: '/tasks/:id',
+    method: 'patch',
+    action: ''
+  },
+  {
+    path: '/tasks/:id',
+    method: 'delete',
+    action: ''
+  },
 ]
