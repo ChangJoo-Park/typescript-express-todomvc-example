0cd0885a846f00e51ac1fb94e07ea722e1dfcfad
ChangJoo Park <pcjpcj2@gmail.com>
2018-02-25 01:22:32 +0900

    Remove console.log

--- src/controller/TaskDeleteAction.ts
+++ src/controller/TaskDeleteAction.ts
@@ -5,13 +5,10 @@ import { Task } from "../entity/Task";
  * Delete one task
  */
 export async function taskDeleteAction(request: Request, response: Response) {
-  console.log('taskDeleteAction : ', request.params)
   const { id } = request.params
-  const task = await Task.findOneById(id)
-
   try {
+    const task = await Task.findOneById(id)
     await task.remove()
-    console.log('remove succeed')
   } catch (error) {
     console.error(error)
   }
